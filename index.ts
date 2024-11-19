da8b6ffa50a8ee361a0ca2a3322c4f20b586f464
huyle <lehuuduchuy@gmail.com>
2024-11-19 20:10:41 +0700

    cors

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
 import mainV1Routes from "./routes/index.route";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ database.connect();
 
 const app: Express = express();
 const port: Number | String = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

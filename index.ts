9082407c3e42764a5efb1ec15e5e2d8ecf303349
huyle <lehuuduchuy@gmail.com>
2024-11-19 09:32:17 +0700

    Danh sach cac cong viec

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("OK");
+import express, {Express, Request, Response} from "express";
+import * as database from "./config/database"
+import dotenv from "dotenv"
+import { Task } from "./models/task.model";
+// import Task from "./models/task.model";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: Number | String = process.env.PORT || 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+    const tasks = await Task.find({
+        deleted: false
+    });
+
+    res.json(tasks);
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
